34ae84d341a0e9fdc8e8c147258f512ff89febc6
Messem Kazim Muhammad <meskaz285@gmail.com>
2023-07-15 14:17:51 +0500

    views added

--- app/src/main/java/com/example/naqcode/PageActivity.java
+++ app/src/main/java/com/example/naqcode/PageActivity.java
@@ -2,13 +2,37 @@ package com.example.naqcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PageActivity extends AppCompatActivity {
+    Button buyButton;
+    Button genButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
+
+        buyButton = findViewById(R.id.buyButton);
+        genButton = findViewById(R.id.genRepButton);
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), BuyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        genButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ReportActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
